4158a50b1144c9fd32a2798298331d0482be79fe
plagunas2 <priscilagunas@gmail.com>
2025-02-04 21:29:17 -0600

    added kick damage (untested)

--- loteriagame/Characters/StateMachine/States/kick.gd
+++ loteriagame/Characters/StateMachine/States/kick.gd
@@ -5,6 +5,14 @@ var idle_state: State
 
 func process_frame(delta: float) -> State:
 	#change state after completing animation
-	if parent.animations.get_frame() == 7:
+	if parent.animations.get_frame() == 3: #may need to change later
 		return idle_state
 	return null
+
+
+
+func _on_hit_box_body_entered(body: Node3D) -> void:
+	if body.get_class() == "Player" && body.get_parent_node_3d() != parent.get_parent_node_3d():
+		print("player has been hit!")
+		if parent.animations.get_frame() == 3 or 4 or 5: #punching frames
+			body.health -= 2
