57bd9b68e6ac2476a03cad159228b6efdba09e0e
plagunas2 <priscilagunas@gmail.com>
2025-02-10 12:22:44 -0600

    fixed kick animation

--- loteriagame/Characters/StateMachine/States/kick.gd
+++ loteriagame/Characters/StateMachine/States/kick.gd
@@ -5,14 +5,12 @@ var idle_state: State
 
 func process_frame(delta: float) -> State:
 	#change state after completing animation
-	if parent.animations.get_frame() == 3: #may need to change later
+	if parent.animations.get_frame() == 5: 
 		return idle_state
 	return null
-
-
 
 func _on_hit_box_body_entered(body: Node3D) -> void:
 	if body.get_class() == "Player" && body.get_parent_node_3d() != parent.get_parent_node_3d():
 		print("player has been hit!")
-		if parent.animations.get_frame() == 3 or 4 or 5: #punching frames
+		if parent.animations.get_frame() == 3 or 4 or 5: #kicking frames
 			body.health -= 2
